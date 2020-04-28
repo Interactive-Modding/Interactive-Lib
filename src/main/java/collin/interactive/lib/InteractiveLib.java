64b94a066d7a9af4d88741d11bfd52207f27c110
Collinvht <collin060204@gmail.com>
2020-04-28 13:36:27 +0200

    Initial Commit

---
+++ src/main/java/collin/interactive/lib/InteractiveLib.java
@@ -0,0 +1,10 @@
+package collin.interactive.lib;
+
+import net.fabricmc.api.ModInitializer;
+
+public class InteractiveLib implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		
+	}
+}
